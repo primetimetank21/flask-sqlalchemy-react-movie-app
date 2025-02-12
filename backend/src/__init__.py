a25ab4b69c0a1e4d9cc1c2cb133e6edaddaede11
TanK <theflashtank21@gmail.com>
2025-02-12 01:40:48 -0500

    feat: added initial login and logout functionality

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,6 +1,8 @@
+from uuid import uuid4
 from pathlib import Path
 from flask import Flask
 from .models import db
+from .routes.auth import login_manager
 
 
 def create_app() -> Flask:
@@ -9,8 +11,11 @@ def create_app() -> Flask:
     DB_PATH: Path = Path(Path.cwd(), ".db.sqlite3")
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = uuid4().hex
 
     db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
 
     with app.app_context():
         db.create_all()
