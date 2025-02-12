b3c13b2d1f61a945ba9f9c728748ff23d698c9ef
TanK <theflashtank21@gmail.com>
2025-02-12 01:39:00 -0500

    refactor: changed username to no longer be unique

--- backend/src/models.py
+++ backend/src/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):  # type: ignore
 
     # Login/Registration information
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
